26c2d648eda71634b1c527fd6c486668008935b9
Xing Zhao <zhaoxing8325@gmail.com>
2022-03-20 10:21:51 -0700

    show api result on the screen

--- GraphQLSwiftUI/ContentView.swift
+++ GraphQLSwiftUI/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var countryEmoji = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(countryEmoji)
+        }
+        .onAppear {
+            Network.shared.apollo.fetch(query: SpecificCountryQuery()) { result in
+                switch result {
+                case .success(let graphQLResult):
+                    DispatchQueue.main.async {
+                        if let emoji = graphQLResult.data?.country?.emoji {
+                            self.countryEmoji = emoji
+                        }
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
     }
 }
 
--- GraphQLSwiftUI/GraphQL/Apollo.swift
+++ GraphQLSwiftUI/GraphQL/Apollo.swift
@@ -9,11 +9,6 @@ import Foundation
 import Apollo
 
 class Network {
-    
     static let shared = Network()
     lazy var apollo = ApolloClient(url: URL(string: "https://countries.trevorblades.com/")!)
-    
-    private init() {
-        
-    }
 }
