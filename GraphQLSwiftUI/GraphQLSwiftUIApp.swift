b5285803b1827c2d0e757e56a301b7ce661a44a6
Xing Zhao <zhaoxing8325@gmail.com>
2022-03-19 22:48:40 -0700

    Initial Commit

---
+++ GraphQLSwiftUI/GraphQLSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLSwiftUIApp.swift
+//  GraphQLSwiftUI
+//
+//  Created by Xing Zhao on 2022-03-19.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
